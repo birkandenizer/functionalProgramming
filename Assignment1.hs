41f0f9b963e5068f4eaa6b36ccdfd8c49d9c968c
Birkan Denizer <denizerb@itu.edu.tr>
2018-02-24 10:55:25 +0300

    Implemented dayOfWeek function in Haskell
    dayOfWeek function takes year, month and day parameters and returns which day of the week the date is.

---
+++ Assignment1.hs
@@ -0,0 +1,10 @@
+dayOfWeek :: Integer -> Integer -> Integer -> Integer
+dayOfWeek y m d = (d + t1 + k + t2 + t3 + 5*j) `mod` 7
+  where
+    m' = if m <= 2 then m+12 else m
+    y' = if m <= 2 then y-1 else y
+    t1 = floor (fromIntegral (13 * (m' + 1)) / 5.0)
+    k = y' `mod` 100
+    t2 = floor (fromIntegral (k) / 4.0)
+    t3 = floor (fromIntegral (j) / 4.0)
+    j = y' `div` 100
