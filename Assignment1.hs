e759b414db4ce97415e370ac975d7b1c74096aab
Birkan Denizer <denizerb@itu.edu.tr>
2018-02-24 11:09:40 +0300

    Implemented sundays1 function
    sundays1 function takes a start year and an end year to calculate the number of Sundays in that period

--- Assignment1.hs
+++ Assignment1.hs
@@ -8,3 +8,15 @@ dayOfWeek y m d = (d + t1 + k + t2 + t3 + 5*j) `mod` 7
     t2 = floor (fromIntegral k / 4.0)
     t3 = floor (fromIntegral j / 4.0)
     j = y' `div` 100
+
+sundays1 :: Integer -> Integer -> Integer
+sundays1 start end = sundays' start 1
+  where
+    sundays' :: Integer -> Integer -> Integer
+    sundays' y m
+      | y > end = 0
+      | otherwise = if dayOfWeek y m 1 == 1 then rest + 1 else rest
+      where
+        nextY = if m == 12 then y + 1 else y
+        nextM = if m < 12 then m + 1 else 1
+        rest = sundays' nextY nextM
