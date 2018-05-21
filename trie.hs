ca69bbd05fde81550fbb9de86be1f2a408863f43
birkandenizer <denizerb@itu.edu.tr>
2018-05-21 20:15:08 +0300

    Initial commit based on Skeleton Code

---
+++ trie.hs
@@ -0,0 +1,27 @@
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe
+import System.Environment
+import System.IO
+import Prelude hiding (Word)
+
+data Trie = Trie {end :: Bool, children :: Map Char Trie}
+type Word = String 
+
+empty :: Trie
+empty = undefined
+
+insert :: Word -> Trie -> Trie 
+insert = undefined
+
+insertList :: [Word] -> Trie 
+insertList = undefined
+
+search :: Word -> Trie -> Bool 
+search = undefined
+
+getWords :: Trie -> [Word] 
+getWords = undefined
+
+prefix :: Word -> Trie -> Maybe [Word] 
+prefix = undefined
