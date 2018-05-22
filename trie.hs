eb478fb62c297588e41f1ef16f9fc0c346bd1f3f
birkandenizer <denizerb@itu.edu.tr>
2018-05-22 05:20:06 +0300

    Implemented main function and helper functions

--- trie.hs
+++ trie.hs
@@ -25,3 +25,51 @@ getWords = undefined
 
 prefix :: Word -> Trie -> Maybe [Word]
 prefix = undefined
+
+addWord :: IO String
+addWord = do
+    getWord
+
+searchWord :: IO String
+searchWord = do
+    getWord
+
+findWord :: IO String
+findWord = do
+    getWord
+
+printAllWords :: IO String
+printAllWords = do
+    getWord
+
+printMenu :: IO String
+printMenu = do
+    putStrLn "a) Add Word"
+    putStrLn "s) Search Word"
+    putStrLn "f) Find words with prefix"
+    putStrLn "p) Print all words"
+    putStrLn "e) Exit"
+    putStrLn "Enter the action:"
+    character <- getChar
+
+    case character of
+        'a' -> addWord
+        's' -> searchWord
+        'f' -> findWord
+        'p' -> printAllWords 
+        'e' -> return "Exit"
+        _ -> return "Wrong action, please try again!"
+
+getWord :: IO String
+getWord = do
+    putStrLn "Enter word/prefix:"
+    line <- getLine
+    return line
+
+main :: IO String
+main = do
+    [arg] <- getArgs
+    content <- readFile arg
+    let separatedLines = lines content
+    let a = insertList separatedLines
+    printMenu
